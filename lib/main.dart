488223116e74089f33b9759a95c24e5176c67acc
Michael Timi <michtimiael@gmail.com>
2020-01-17 11:31:28 +0100

    New Homepage Stateless Widget Created

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
